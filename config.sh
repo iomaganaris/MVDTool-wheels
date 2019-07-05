ae08b35ed24f1aeacd3804f63ba4c116eb1635a5
Ioannis Magkanaris <iomagkanaris@gmail.com>
2019-07-05 15:05:18 +0200

    First commit

---
+++ config.sh
@@ -0,0 +1,17 @@
+# Define custom utilities
+# Test for macOS with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    if [ -n "$IS_OSX" ]; then
+        build_hdf5
+    fi
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    python -c 'import sys; import morphio; print(morphio.version)'
+}
+
