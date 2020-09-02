757bf148b6bb8785c64d903fc3ea09c3d18d6d95
Ioannis Magkanaris <iomagkanaris@gmail.com>
2020-09-02 16:20:28 +0200

    Update .travis.yaml and config.sh according to MorphIO-wheels

--- config.sh
+++ config.sh
@@ -1,17 +1,28 @@
-# Define custom utilities
-# Test for macOS with [ -n "$IS_OSX" ]
+set -x
+
+function build_cmake {
+    build_simple cmake 3.7.2 https://cmake.org/files/v3.7/
+}
 
 function pre_build {
-    # Any stuff that you need to do before you start building the wheels
-    # Runs in the root directory of this repository.
-    if [ -n "$IS_OSX" ]; then
+    if [ -z "$IS_OSX" ]; then
+        build_cmake
         build_hdf5
+
+        # Add workaround for auditwheel bug:
+        # https://github.com/pypa/auditwheel/issues/29
+        local bad_lib="/usr/local/lib/libhdf5.so"
+        if [ -z "$(readelf --dynamic $bad_lib | grep RUNPATH)" ]; then
+            echo "Patching $bad_lib"
+            patchelf --set-rpath $(dirname $bad_lib) $bad_lib
+        fi
+    else
+        brew list cmake || brew install cmake
+        brew list hdf5 || brew install hdf5
     fi
 }
 
 function run_tests {
-    # Runs tests on installed distribution from an empty directory
     python --version
     python -c 'import sys; import morphio; print(morphio.version)'
 }
-
